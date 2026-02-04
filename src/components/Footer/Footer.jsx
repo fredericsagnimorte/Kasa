dd6301a3be7bf31bde88b2519bada3d282f99464
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-02-04 16:35:00 +0100

    feat: Add footer component with logo and styles

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <img src="./src/assets/images/footer_LOGO.svg" alt="Logo Kasa" />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Router>
             <Header />
-            <Routes>
-                <Route path="/" element=""></Route>
-                <Route path="/a_propos" element=""></Route>
-                <Route path="/:id_appartement" element=""></Route>
-                <Route path="*" element=""></Route>
-            </Routes>
+            <main>
+                <Routes>
+                    <Route path="/" element=""></Route>
+                    <Route path="/a_propos" element=""></Route>
+                    <Route path="/:id_appartement" element=""></Route>
+                    <Route path="*" element=""></Route>
+                </Routes>
+            </main>
+            <Footer />
         </Router>
     </StrictMode>,
 );
