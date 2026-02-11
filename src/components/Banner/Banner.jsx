c92da6f7885b484beeb86dae98c092627ad315d7
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-02-11 18:08:35 +0100

    feat: Add css for header, footer and home, modify Banner to use css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,19 @@
 function Banner({ bannerImg, bannerText = "" }) {
+    const [firstPart, secondPart] = bannerText.split(",");
+
     return (
         <div className="banner">
             <img
-                className="banner__image"
+                className="banner__img"
                 src={bannerImg}
                 alt="Baniere paysage"
             />
-            {bannerText != "" && <p className="banner__text">{bannerText}</p>}
+            {bannerText != "" && (
+                <p className="banner__text">
+                    {firstPart},
+                    <span className="mobile-break">{secondPart}</span>
+                </p>
+            )}
         </div>
     );
 }
