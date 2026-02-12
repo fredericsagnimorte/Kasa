7f510bb398d41bae47da8dde980a38b41642ed8a
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-02-12 11:04:37 +0100

    feat: Enhance Error page styles for better responsiveness; update text layout

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -7,7 +7,10 @@ function Error() {
                 <span className="errorMessage__404">404</span>
             </p>
             <p className="errorMessage__pageDoNotExist">
-                Oups! La page que vous demandez n'existe pas.
+                Oups! La page que{" "}
+                <span className="mobile-break">
+                    vous demandez n'existe pas.
+                </span>
             </p>
             <Link className="errorMessage__backHome" to="/">
                 Retourner sur la page d’accueil
