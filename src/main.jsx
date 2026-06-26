ea8f91fc3e911006bf71aedc46cd1edb2ad7c161
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-06-26 17:05:29 +0200

    fix: Set basename for Router to ensure correct routing in Kasa application

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import About from "./pages/About/About";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Router>
+        <Router basename="/Kasa">
             <Header />
             <main>
                 <Routes>
